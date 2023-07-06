05df3629090238ba358a0bd29450007ffd044e97
Aashish Kaushik <70471873+Aashish-Kaushik@users.noreply.github.com>
2023-07-06 05:59:59 +0000

    create model for blog

---
+++ models/blogModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    content: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+
+    },
+    userId: {
+        type: String,
+
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
