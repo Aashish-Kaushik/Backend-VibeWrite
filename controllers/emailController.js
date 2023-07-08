5c205b0e1ff32ac6e63d46852b08e1cc086ff80f
Aashish-Kaushik <ashishkaushik8126@gmail.com>
2023-07-08 21:46:00 +0530

    ✨ add email functionality

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,1 +1,42 @@
-//  setup mail sender for contact us 
+//  setup mail sender for contact us
+
+const nodeMailer = require("nodemailer");
+
+const transporter = nodeMailer.createTransport({
+  host: smtp.forwardemail.net,
+  port: 465,
+  secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: "Replace with userName ",
+    pass: "Replace with pass",
+  },
+});
+
+exports.mailsenders = async (req, res) => {
+  try {
+    const { email, body, subject } = req.body;
+    const info = await transporter
+      .sendMail({
+        from: " PLease add sender_id",
+        to: email,
+        subject: subject,
+        text: body,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    res.send({
+      status: "success",
+      response: info,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+    });
+  }
+};
